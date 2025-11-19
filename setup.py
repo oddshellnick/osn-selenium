29ff13773723f665503bf3dcd347c46cf87cc732
Eduard Dik <oddshellnick.programming@gmail.com>
2025-11-19 16:30:47 +0700

    updated package version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_description() -> str:
 
 setup(
 		name="osn-selenium",
-		version="0.0.1-beta.2",
+		version="0.0.1-beta.3",
 		author="oddshellnick",
 		author_email="oddshellnick.programming@gmail.com",
 		description=get_description(),
