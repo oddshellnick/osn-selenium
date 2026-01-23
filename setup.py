657c2e27e28a6125bae8a088edf24f1ca67e2728
Eduard Dik <oddshellnick.programming@gmail.com>
2026-01-24 00:41:02 +0700

    osn-selenium v1.0.0-beta.7 release commit

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_description() -> str:
 
 setup(
 		name="osn-selenium",
-		version="1.0.0-beta.6",
+		version="1.0.0-beta.7",
 		author="oddshellnick",
 		author_email="oddshellnick.programming@gmail.com",
 		description=get_description(),
