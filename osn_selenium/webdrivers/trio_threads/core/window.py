37db741a244128546ce3eabe97fd8042fd897302
Eduard Dik <oddshellnick.programming@gmail.com>
2026-01-29 13:09:05 +0700

    changed `switch_to` method from async to sync

--- osn_selenium/webdrivers/trio_threads/core/window.py
+++ osn_selenium/webdrivers/trio_threads/core/window.py
@@ -92,8 +92,8 @@ class CoreWindowMixin(UnifiedCoreWindowMixin, TrioThreadMixin, AbstractCoreWindo
 	async def set_window_size(self, width: int, height: int, windowHandle: str = "current") -> None:
 		await self.sync_to_trio(sync_function=self._set_window_size_impl)(width=width, height=height, windowHandle=windowHandle)
 	
-	async def switch_to(self) -> SwitchTo:
-		legacy = await self.sync_to_trio(sync_function=self._switch_to_impl)()
+	def switch_to(self) -> SwitchTo:
+		legacy = self._switch_to_impl()
 		return get_trio_thread_instance_wrapper(
 				wrapper_class=SwitchTo,
 				legacy_object=legacy,
